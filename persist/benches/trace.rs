00c96d0863129b4f0f9f54a7601477820a40eebb
Daniel Harrison <52528+danhhz@users.noreply.github.com>
2021-04-27 15:41:47 -0700

    reorg. add OrdValBuilder benches for comparison
        get_key                 time:   [45.094 ns 45.183 ns 45.270 ns]
        seek_key                time:   [793.85 ns 797.07 ns 801.41 ns]
        get_key #2              time:   [1.8604 ns 1.8609 ns 1.8613 ns]
        seek_key #2             time:   [49.790 ns 49.858 ns 49.935 ns]

---
+++ persist/benches/trace.rs
@@ -0,0 +1,125 @@
+//! Benchmarks for capnp based batch persistence.
+
+use capnp::message::ReaderOptions;
+use capnp::serialize_packed;
+use criterion::{criterion_group, criterion_main, Criterion};
+use pprof::criterion::{Output, PProfProfiler};
+
+use differential_dataflow::trace::implementations::ord::{OrdValBatch, OrdValBuilder};
+use differential_dataflow::trace::{BatchReader, Builder, Cursor};
+use persist::trace::{capnpgen, PBatch};
+use timely::progress::Antichain;
+
+fn gen_tuples<F: FnMut(u32, String, String, u64, i64)>(tuples: u32, mut set_tuple: F) {
+    const TUPLES_PER_VAL: u32 = 2;
+    const VALS_PER_KEY: u32 = 2;
+    let keys = tuples / TUPLES_PER_VAL / VALS_PER_KEY;
+    let key_len = (keys as f64).log(16.0) as u32 + 1;
+
+    for tuple_idx in 0..tuples {
+        let key_idx = tuple_idx / (TUPLES_PER_VAL * VALS_PER_KEY);
+        let key = format!("{:0width$x}", key_idx, width = key_len as usize);
+        let val_idx = (tuple_idx % (TUPLES_PER_VAL * VALS_PER_KEY)) / VALS_PER_KEY;
+        let val = format!("{}-{:x}", key, val_idx);
+        set_tuple(tuple_idx, key, val, tuple_idx.into(), 1)
+    }
+}
+
+fn gen_capnp(tuples: u32) -> Vec<u8> {
+    let mut message = ::capnp::message::Builder::new_default();
+    {
+        let batch = message.init_root::<capnpgen::batch::Builder>();
+        let mut t = batch.init_tuples(tuples as u32);
+        gen_tuples(
+            tuples,
+            |idx: u32, key: String, val: String, ts: u64, diff: i64| {
+                let mut tuple = t.reborrow().get(idx);
+                tuple.set_key(key.as_bytes());
+                tuple.set_val(val.as_bytes());
+                tuple.set_ts(ts);
+                tuple.set_diff(diff);
+            },
+        );
+    }
+    let mut buf = vec![];
+    serialize_packed::write_message(&mut buf, &message).expect("writes to Vec<u8> are infallable");
+    buf
+}
+
+fn gen_mem(tuples: u32) -> OrdValBatch<String, String, u64, i64, u32> {
+    let mut batch = OrdValBuilder::with_capacity(tuples as usize);
+    gen_tuples(
+        tuples,
+        |_idx: u32, key: String, val: String, ts: u64, diff: i64| {
+            batch.push((key, val, ts, diff));
+        },
+    );
+    batch.done(
+        Antichain::from_elem(0),
+        Antichain::from_elem(0),
+        Antichain::from_elem(0),
+    )
+}
+
+fn capnp_benchmark(c: &mut Criterion) {
+    let batch = gen_capnp(1000000);
+
+    c.bench_function("decode", |b| {
+        b.iter(|| {
+            let message_reader =
+                serialize_packed::read_message(&mut &batch[..], ReaderOptions::new()).unwrap();
+            let b = message_reader
+                .get_root::<capnpgen::batch::Reader>()
+                .unwrap();
+            let _ = PBatch::from_reader(b).unwrap();
+        })
+    });
+
+    let message_reader = serialize_packed::read_message(
+        &mut &batch[..],
+        *ReaderOptions::new().traversal_limit_in_words(None), // Well this is unfortunate
+    )
+    .unwrap();
+    let batch = message_reader
+        .get_root::<capnpgen::batch::Reader>()
+        .unwrap();
+    let batch = PBatch::from_reader(batch).unwrap();
+    c.bench_function("get_key", |b| {
+        b.iter(|| {
+            let c = batch.cursor();
+            assert!(c.get_key(&batch).is_some());
+        })
+    });
+    c.bench_function("seek_key", |b| {
+        b.iter(|| {
+            let mut c = batch.cursor();
+            c.seek_key(&batch, &b"0000b"[..]);
+            assert!(c.get_key(&batch).is_some());
+        })
+    });
+}
+
+fn mem_benchmark(c: &mut Criterion) {
+    let batch = gen_mem(1000000);
+
+    c.bench_function("get_key", |b| {
+        b.iter(|| {
+            let c = batch.cursor();
+            assert!(c.get_key(&batch).is_some());
+        })
+    });
+    c.bench_function("seek_key", |b| {
+        b.iter(|| {
+            let mut c = batch.cursor();
+            c.seek_key(&batch, &"0000b".to_string());
+            assert!(c.get_key(&batch).is_some());
+        })
+    });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets = capnp_benchmark, mem_benchmark
+}
+criterion_main!(benches);
--- persist/build.rs
+++ persist/build.rs
@@ -2,7 +2,7 @@ fn main() {
     capnpc::CompilerCommand::new()
         .src_prefix("src")
         .file("src/trace.capnp")
-        .default_parent_module(vec!["capnpgen".into()])
+        .default_parent_module(vec!["trace".into(), "capnpgen".into()])
         .run()
         .expect("capnp schema compilation");
 }
